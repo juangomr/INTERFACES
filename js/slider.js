e7ec4dc09501da78d535e108376a1d667a993adc
CEP-AULA7-PC13\TECH <juangomr@gmail.com>
2024-01-09 20:31:01 +0100

    09/01

--- js/slider.js
+++ js/slider.js
@@ -1,27 +1,23 @@
-var swiper = new Swiper('.swiper-container', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: 3,
-    spaceBetween: 10,
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 10,
 
-    breakpoints: {
-        620: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-        },
-        680: {
-            slidesrPerView: 2,
-            spaceBetween: 20,
-        },
-        920: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-        },
-        1240: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        }
-    }
+  breakpoints: {
+    620: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    680: {
+      slidesrPerView: 2,
+      spaceBetween: 20,
+    },
+    920: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1240: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
 });
