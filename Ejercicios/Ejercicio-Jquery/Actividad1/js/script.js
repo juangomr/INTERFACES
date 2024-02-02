f268c7750ff026483f59b443dd7adaf7fc711cf5
Juangomr <juangomr@gmail.com>
2024-02-02 11:11:13 +0100

    02/02

--- Ejercicios/Ejercicio-Jquery/Actividad1/js/script.js
+++ Ejercicios/Ejercicio-Jquery/Actividad1/js/script.js
@@ -1,6 +1,21 @@
+$(".main2").hide();
+
 $(document).ready(function () {
   $(".iniciar").click(function () {
-    $(".main").accordion();
     $(".main").hide();
+    $(".main2").show();
   });
+
+  $(".iniciarSesion").prop("disabled", true);
+
+  $(".form-control1, .form-control2").on("input", function () {
+
+    if ($(this).val() == "" ) {
+      $(".iniciarSesion").prop("disabled", true);
+    } else {
+      $(".iniciarSesion").prop("disabled", false);
+    }
+  })
+
+
 });
